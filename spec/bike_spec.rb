9f1342dc1de1ba58bb8390cffa18d0341981af72
Marc Blenkinsop <marcblenky@hotmail.com>
2019-10-11 15:32:20 +0100

    added bike class and spec files

---
+++ spec/bike_spec.rb
@@ -0,0 +1,15 @@
+require 'bike'
+
+describe Bike do
+  before :each do
+    @bike = Bike.new
+  end
+
+  it 'instantiates an instance of the Bike class' do
+    expect(@bike).to be_kind_of(Bike)
+  end
+
+  it 'bike instance responds to working? method' do
+    expect(@bike).to respond_to(:working?)
+  end
+end
