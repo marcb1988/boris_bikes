4a0ca10febffad2888f6f6af4e437545ca8db9bb
Marc Blenkinsop <marcblenky@hotmail.com>
2019-10-11 16:41:59 +0100

    :construction: Added methods for dock bike and began editing release_bike

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -1,7 +1,17 @@
 require "bike"
 
 class DockingStation
+  attr_reader :bikes
+  def initialize
+    @bikes = []
+  end
+
   def release_bike
-    Bike.new
+    result = @bikes.empty? ? (raise "No Bikes") : Bike.new
+    result
+  end
+
+  def dock_bike(bike)
+    @bikes << bike
   end
 end
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,4 +1,5 @@
 require "docking_station"
+require "bike"
 
 describe DockingStation do
   before :each do
@@ -15,5 +16,18 @@ describe DockingStation do
 
   it "DockingStation can release a bike" do
     expect(@docking_station.release_bike).to be_truthy
+  end
+
+  it "release_bike returns error if @bikes is empty" do
+    expect{ raise "No Bikes"}.to raise_error
+  end
+
+  it "DockingStation responds to dock_bike" do
+    expect(@docking_station).to respond_to(:dock_bike)
+  end
+
+  it "DockingStation can dock a bike" do
+    @docking_station.dock_bike(@bike)
+    expect(@docking_station.bikes).to include(@bike)
   end
 end
