77d6b37580fe08ab363541e55a586f812be82b0c
Marc Blenkinsop <marcblenky@hotmail.com>
2019-10-16 15:13:30 +0100

    committed updated code for Van class

---
+++ lib/ven.rb
@@ -0,0 +1,13 @@
+class Van
+  def pick_up_broken
+  end
+
+  def drop_off_broken
+  end
+
+  def pick_up_fixed
+  end
+
+  def drop_off_fixed
+  end
+end
---
+++ spec/ven_spec.rb
@@ -0,0 +1,13 @@
+describe Van do
+  it "picks up broken bikes" do
+  end
+
+  it "delivers broken bikes to garage" do
+  end
+
+  it "picks up fixed bikes from garage" do
+  end
+
+  it "delivers fixed bikes to DockingStation" do
+  end
+end
